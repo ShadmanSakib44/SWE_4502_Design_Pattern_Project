27751c06f66241621ce13f0e997cab40876cb992
ShadmanSakib44 <shadmansakibiut@gmail.com>
2023-11-09 18:03:26 +0600

    Starting the project

--- SWE_4502_Design_Pattern_Project/src/main/java/cars/ConcreteCars/ConcreteCarA.java
+++ SWE_4502_Design_Pattern_Project/src/main/java/cars/ConcreteCars/ConcreteCarA.java
@@ -1,4 +1,18 @@
 package cars.ConcreteCars;
 
-public class ConcreteCarA {
+import cars.Car;
+import cars.EngineStrategy;
+import cars.TurbochargerStrategy;
+
+public class ConcreteCarA extends Car {
+
+    public ConcreteCarA(EngineStrategy engineStrategy, TurbochargerStrategy turbochargerStrategy) {
+        super(engineStrategy, turbochargerStrategy);
+    }
+
+    @Override
+    public void displayCarInfo() {
+        System.out.println("ConcreteCarA with custom engine and turbocharger settings.");
+
+    }
 }
--- SWE_4502_Design_Pattern_Project/src/main/java/cars/ConcreteCars/ConcreteCarB.java
+++ SWE_4502_Design_Pattern_Project/src/main/java/cars/ConcreteCars/ConcreteCarB.java
@@ -1,4 +1,18 @@
 package cars.ConcreteCars;
 
-public class ConcreteCarB {
+import cars.Car;
+import cars.EngineStrategy;
+import cars.TurbochargerStrategy;
+
+public class ConcreteCarB extends Car {
+
+    public ConcreteCarB(EngineStrategy engineStrategy, TurbochargerStrategy turbochargerStrategy) {
+        super(engineStrategy, turbochargerStrategy);
+    }
+
+    @Override
+    public void displayCarInfo() {
+        System.out.println("ConcreteCarB with custom engine and turbocharger settings.");
+        
+    }
 }
